d898d339baa605002d7c9debcc7efaf782e0b971
Gerardo Garcia <1ggarcia101@gmail.com>
2023-07-24 16:50:10 -0700

    added models

---
+++ CityInfoAPI/CityInfoAPI/CitiesDataStore.cs
@@ -0,0 +1,35 @@
+﻿using CityInfoAPI.Models;
+
+namespace CityInfoAPI
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The One with the big park."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with the tower."
+                },
+            };
+        }
+    }
+}
